eda4b925bdaf3fea1237162de41d1bd01a1bd2cc
onyangoouma <jacksonouma04@gmail.com>
2022-10-13 00:04:24 +0300

    created a CRUD method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,42 @@
 class ReviewsController < ApplicationController
+    #READ/reviews
+    def index 
+        render json: Review.all, status: :ok
+    end
+
+    #READ/reviews/:id
+    def show
+        review = Review.find(params[:id])
+        render json: review, status: :ok
+    end
+
+    #POST /reviews
+    def create
+        review = Review.create!(review_params)
+
+        render json: review, status: :created
+    end
+
+    #PATCH /reviews/:id
+    def update
+        review = Review.find(params[:id])
+        review.update(review_params)
+        render json: review
+    end
+
+    #DELETE/reviews/:id
+    def destroy
+        review = Review.find(params[:id])
+        review.destroy
+        head :no_content
+    end
+
+    private
+
+    def review_params
+        params.permit(:text, :user_id, :user_park_id)
+    end
+
+end
+
 end
